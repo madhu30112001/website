a697fb3535125af9bd78e5c9eefa20c9c262d7d7
madhu30112001 <madhushirur3@gmail.com>
2024-10-19 23:15:35 +0530

    removed uri from db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectdb=async()=>{
-    (await mongoose.connect('mongodb+srv://madhu:Madhu30042001@cluster0.zxh2m.mongodb.net/booking-app').then(()=>console.log("DB Connected")));
+    (await mongoose.connect('your mongo db uri').then(()=>console.log("DB Connected")));
 
     
 }
