a9899af074773bf4b76c8c2e9a95aa76d964d95c
madhu30112001 <madhushirur3@gmail.com>
2024-10-29 19:24:25 +0530

    adding files again

--- food_app_backend/config/db.js
+++ food_app_backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // const mongoose = require("mongoose");
 
 export const connectDB = async () => {
-  await mongoose.connect('mongodb://127.0.0.1:27017/FoodDel')
+  await mongoose.connect('')
     .then(() => {
       console.log('DB connected');
     })
